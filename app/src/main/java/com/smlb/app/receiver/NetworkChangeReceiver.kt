dccb6ed9efd2fdeba0eff517dc262747d6dcb921
summer <sunmeng1995@outlook.com>
2017-08-07 18:04:52 +0800

    Summer,add receiver

---
+++ app/src/main/java/com/smlb/app/receiver/NetworkChangeReceiver.kt
@@ -0,0 +1,17 @@
+package com.smlb.app.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+/**
+ * Created by Sunmeng on 8/7/2017.
+ * E-Mail:Sunmeng1995@outlook.com
+ * Description:
+ */
+
+class NetworkChangeReceiver : BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
